d2dea28860e779d26029250ecc3f0b0d32e992f4
lore <lorenzomartino86@gmail.com>
2018-05-12 14:02:02 +0200

    Add React dependency

---
+++ ui/src/main/webapp/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
